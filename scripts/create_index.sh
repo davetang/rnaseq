64eab8dd3378ab08e31dbf359b83fa65531564f6
Dave Tang <davetingpongtang@gmail.com>
2024-08-22 13:43:47 +0900

    Update script

--- scripts/create_index.sh
+++ scripts/create_index.sh
@@ -2,47 +2,63 @@
 
 set -euo pipefail
 
-num_threads=8
-gtf=chrX_data/genes/gencode.v37.annotation.chrx.gtf
-overhang=75
+DIR=$(dirname $(realpath $0))
+DATA_DIR=${DIR}/../raw
+BIN_DIR=${DIR}/../bin
+CHRX_DIR=${DIR}/../raw/chrX_data
 
-if [[ ! -d chrX_data/indexes/star_index_${overhang} ]]; then
+RSEM_VER=1.3.3
+HISAT2_VER=2.2.1
+GENCODE_VER=46
+
+THREADS=4
+GTF=${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.chrx.gtf
+OVERHANG=75
+
+if [[ ! -d ${CHRX_DIR}/indexes/star_index_${OVERHANG} ]]; then
    >&2 echo Building STAR index
-   ../src/STAR \
+   ${BIN_DIR}/STAR \
       --runMode genomeGenerate \
-      --genomeDir chrX_data/indexes/star_index_${overhang} \
-      --genomeFastaFiles chrX_data/genome/chrX.fa \
+      --genomeDir ${CHRX_DIR}/indexes/star_index_${OVERHANG} \
+      --genomeFastaFiles ${CHRX_DIR}/genome/chrX.fa \
       --genomeSAindexNbases 12 \
-      --sjdbGTFfile ${gtf} \
-      --sjdbOverhang ${overhang} \
-      --runThreadN ${num_threads}
+      --sjdbGTFfile ${GTF} \
+      --sjdbOverhang ${OVERHANG} \
+      --runThreadN ${THREADS}
+else
+   >&2 echo ${CHRX_DIR}/indexes/star_index_${OVERHANG} already exists
 fi
 
-if [[ ! -d chrX_data/indexes/rsem_reference ]]; then
+if [[ ! -d ${CHRX_DIR}/indexes/rsem_reference ]]; then
    >&2 echo Building RSEM index
-   mkdir -p chrX_data/indexes/rsem_reference
-   ../src/RSEM-1.3.3/rsem-prepare-reference \
-      chrX_data/genome/chrX.fa \
-      chrX_data/indexes/rsem_reference/chrX \
-      --gtf ${gtf} \
-      --num-threads ${num_threads}
+   mkdir -p ${CHRX_DIR}/indexes/rsem_reference
+   ${BIN_DIR}/RSEM-${RSEM_VER}/rsem-prepare-reference \
+      ${CHRX_DIR}/genome/chrX.fa \
+      ${CHRX_DIR}/indexes/rsem_reference/chrX \
+      --gtf ${GTF} \
+      --num-threads ${THREADS}
+else
+   >&2 echo ${CHRX_DIR}/indexes/rsem_reference already exists
 fi
 
-if [[ ! -d chrX_data/indexes/hisat2_index ]]; then
-   >&2 echo Building HISAT2 index
-   mkdir chrX_data/indexes/hisat2_index
-   ../src/hisat2-2.2.0/extract_splice_sites.py ${gtf} > chrX.ss
-   ../src/hisat2-2.2.0/extract_exons.py ${gtf} > chrX.exon
-   ../src/hisat2-2.2.0/hisat2-build -p ${num_threads} --ss chrX.ss --exon chrX.exon chrX_data/genome/chrX.fa chrX_data/indexes/hisat2_index/chrX_tran
-   rm chrX.ss chrX.exon
-fi
-
-if [[ ! -d chrX_data/indexes/kallisto_index ]]; then
+if [[ ! -d ${CHRX_DIR}/indexes/kallisto_index ]]; then
    >&2 echo Building kallisto index
-   mkdir chrX_data/indexes/kallisto_index
-   ../src/kallisto/kallisto index -i chrX_data/indexes/kallisto_index/transcripts.idx chrX_data/genes/gencode.v37.transcripts.chrx.fa
+   mkdir ${CHRX_DIR}/indexes/kallisto_index
+   ${BIN_DIR}/kallisto/kallisto index -i ${CHRX_DIR}/indexes/kallisto_index/transcripts.idx ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.transcripts.chrx.fa
+else
+   >&2 echo ${CHRX_DIR}/indexes/kallisto_index already exists
+fi
+
+if [[ ! -d ${CHRX_DIR}/indexes/hisat2_index ]]; then
+   >&2 echo Building HISAT2 index
+   mkdir ${CHRX_DIR}/indexes/hisat2_index
+   ${BIN_DIR}/hisat2-${HISAT2_VER}/extract_splice_sites.py ${GTF} > chrX.ss
+   ${BIN_DIR}/hisat2-${HISAT2_VER}/extract_exons.py ${GTF} > chrX.exon
+   ${BIN_DIR}/hisat2-${HISAT2_VER}/hisat2-build -p ${THREADS} --ss chrX.ss --exon chrX.exon ${CHRX_DIR}/genome/chrX.fa ${CHRX_DIR}/indexes/hisat2_index/chrX_tran
+   rm chrX.ss chrX.exon
+else
+   >&2 echo ${CHRX_DIR}/indexes/hisat2_index already exists
 fi
 
 >&2 echo Done
 exit 0
-
