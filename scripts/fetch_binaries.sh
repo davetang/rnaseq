5bfd1f374bd2f5060949022001562226d2579d13
Dave Tang <davetingpongtang@gmail.com>
2024-08-22 12:10:20 +0900

    Fetch binaries only for amd64/x86_64

--- scripts/fetch_binaries.sh
+++ scripts/fetch_binaries.sh
@@ -15,65 +15,44 @@ for tool in ${dependencies[@]}; do
    check_depend ${tool}
 done
 
-case ${OSTYPE} in
-   darwin*)
-      >&2 echo Downloading binaries for macOS
-      if [[ ! -d hisat2-2.2.0 ]]; then
-         wget -q -c https://cloud.biohpc.swmed.edu/index.php/s/hisat2-220-OSX_x86_64/download -O hisat2-2.2.0-OSX_x86_64.zip
-         unzip -q hisat2-2.2.0-OSX_x86_64.zip
-         rm hisat2-2.2.0-OSX_x86_64.zip
-         >&2 echo Finished downloading hisat2
-      fi
-      if [[ ! -d stringtie-2.1.4 ]]; then
-         wget -q -c http://ccb.jhu.edu/software/stringtie/dl/stringtie-2.1.4.OSX_x86_64.tar.gz
-         tar -xzf stringtie-2.1.4.OSX_x86_64.tar.gz
-         mv stringtie-2.1.4.OSX_x86_64 stringtie-2.1.4
-         rm stringtie-2.1.4.OSX_x86_64.tar.gz
-         >&2 echo Finished downloading stringtie
-      fi
-      if [[ ! -e STAR ]]; then
-         wget -q -c https://github.com/alexdobin/STAR/blob/master/bin/MacOSX_x86_64/STAR?raw=true -O STAR
-         chmod 755 STAR
-         >&2 echo Finished downloading STAR
-      fi
-      if [[ ! -d kallisto ]]; then
-         wget -q -c https://github.com/pachterlab/kallisto/releases/download/v0.46.2/kallisto_mac-v0.46.2.tar.gz
-         tar -xzf kallisto_mac-v0.46.2.tar.gz
-         rm kallisto_mac-v0.46.2.tar.gz
-         >&2 echo Finished downloading kallisto
-      fi
-   ;; 
-   linux*)
-      >&2 echo Downloading binaries for Linux
-      if [[ ! -d hisat2-2.2.0 ]]; then
-         wget -q -c https://cloud.biohpc.swmed.edu/index.php/s/hisat2-220-Linux_x86_64/download -O hisat2-2.2.0-Linux_x86_64.zip
-         unzip -q hisat2-2.2.0-Linux_x86_64.zip
-         rm hisat2-2.2.0-Linux_x86_64.zip
-         >&2 echo Finished downloading hisat2
-      fi
-      if [[ ! -d stringtie-2.1.4.Linux_x86_64 ]]; then
-         wget -q -c http://ccb.jhu.edu/software/stringtie/dl/stringtie-2.1.4.Linux_x86_64.tar.gz
-         tar -xzf stringtie-2.1.4.Linux_x86_64.tar.gz
-         mv stringtie-2.1.4.Linux_x86_64 stringtie-2.1.4
-         rm stringtie-2.1.4.Linux_x86_64.tar.gz
-         >&2 echo Finished downloading stringtie
-      fi
-      if [[ ! -e STAR ]]; then
-         wget -q -c https://github.com/alexdobin/STAR/blob/master/bin/Linux_x86_64/STAR?raw=true -O STAR
-         chmod 755 STAR
-         >&2 echo Finished downloading STAR
-      fi
-      if [[ ! -d kallisto ]]; then
-         wget -q -c https://github.com/pachterlab/kallisto/releases/download/v0.46.2/kallisto_linux-v0.46.2.tar.gz
-         tar -xzf kallisto_linux-v0.46.2.tar.gz
-         rm kallisto_linux-v0.46.2.tar.gz
-         >&2 echo Finished downloading kallisto
-      fi
-   ;;
-  *)
-      >&2 echo Not sure which binaries to download for ${OSTYPE}
-   ;;
-esac
+DIR=$(dirname $(realpath $0))
+BIN_DIR=${DIR}/../bin
+
+if [[ ! -d ${BIN_DIR} ]]; then
+   mkdir ${BIN_DIR}
+fi
+
+if [[ ! -d ${BIN_DIR}/hisat2-2.2.1 ]]; then
+   wget -c https://cloud.biohpc.swmed.edu/index.php/s/oTtGWbWjaxsQ2Ho/download -O ${BIN_DIR}/hisat2-2.2.1-Linux_x86_64.zip
+   unzip -d ${BIN_DIR} ${BIN_DIR}/hisat2-2.2.1-Linux_x86_64.zip
+   rm ${BIN_DIR}/hisat2-2.2.1-Linux_x86_64.zip
+else
+   >&2 echo ${BIN_DIR}/hisat2-2.2.1 already exists
+fi
+
+if [[ ! -d ${BIN_DIR}/stringtie-2.2.3.Linux_x86_64 ]]; then
+   wget -c http://ccb.jhu.edu/software/stringtie/dl/stringtie-2.2.3.Linux_x86_64.tar.gz -O ${BIN_DIR}/stringtie-2.2.3.Linux_x86_64.tar.gz
+   tar --directory ${BIN_DIR} -xzf ${BIN_DIR}/stringtie-2.2.3.Linux_x86_64.tar.gz
+   rm ${BIN_DIR}/stringtie-2.2.3.Linux_x86_64.tar.gz
+else
+   >&2 echo ${BIN_DIR}/stringtie-2.2.3.Linux_x86_64 already exists
+fi
+
+if [[ ! -e ${BIN_DIR}/STAR ]]; then
+   wget -c https://github.com/alexdobin/STAR/blob/master/bin/Linux_x86_64_static/STAR?raw=true -O ${BIN_DIR}/STAR
+   chmod 755 ${BIN_DIR}/STAR
+else
+   >&2 echo ${BIN_DIR}/STAR already exists
+fi
+
+KALLISTO_VER=0.50.1
+if [[ ! -d ${BIN_DIR}/kallisto ]]; then
+   wget -c https://github.com/pachterlab/kallisto/releases/download/v${KALLISTO_VER}/kallisto_linux-v${KALLISTO_VER}.tar.gz -O ${BIN_DIR}/kallisto_linux-v${KALLISTO_VER}.tar.gz
+   tar --directory ${BIN_DIR} -xzf ${BIN_DIR}/kallisto_linux-v${KALLISTO_VER}.tar.gz
+   rm ${BIN_DIR}/kallisto_linux-v${KALLISTO_VER}.tar.gz
+else
+   >&2 echo ${BIN_DIR}/kallisto already exists
+fi
 
 >&2 echo Done
-
+exit
