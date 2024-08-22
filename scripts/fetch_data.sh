685e630ac72e19f268ed652adff35c5a1d118984
Dave Tang <davetingpongtang@gmail.com>
2024-08-22 09:22:56 +0900

    Rewrite

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -2,26 +2,42 @@
 
 set -euo pipefail
 
-if [[ ! -e chrX_data.tar.gz ]]; then
-   wget -c https://davetang.org/file/chrX_data.tar.gz
-   tar xvzf chrX_data.tar.gz
+DIR=$(dirname $(realpath $0))
+DATA_DIR=${DIR}/../raw
+CHRX_DIR=${DIR}/../raw/chrX_data
+
+if [[ ! -d ${CHRX_DIR} ]]; then
+   wget -c https://davetang.org/file/chrX_data.tar.gz -O ${DATA_DIR}/chrX_data.tar.gz
+   tar --directory ${DATA_DIR} -xzf ${DATA_DIR}/chrX_data.tar.gz
+   rm ${DATA_DIR}/chrX_data.tar.gz
+else
+   >&2 echo ${CHRX_DIR} already exists
 fi
 
-ver=37
+GENCODE_VER=46
 
-if [[ ! -e chrX_data/genes/gencode.v${ver}.annotation.gtf ]]; then
-   wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${ver}/gencode.v${ver}.annotation.gtf.gz
-   gunzip gencode.v${ver}.annotation.gtf.gz
-   cat gencode.v${ver}.annotation.gtf | grep "^chrX" > gencode.v${ver}.annotation.chrx.gtf
-   cat gencode.v37.annotation.chrx.gtf | perl -lane 'next unless $F[2] eq "transcript"; $tid = $1 if /transcript_id\s\"(.*?)\";/; print "$tid"' > gencode.v37.annotation.chrx.tid.txt
-   mv gencode.v${ver}.annotation.gtf gencode.v${ver}.annotation.chrx.gtf gencode.v37.annotation.chrx.tid.txt chrX_data/genes/
+if [[ ! -e ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.gtf ]]; then
+   wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${GENCODE_VER}/gencode.v${GENCODE_VER}.annotation.gtf.gz -O ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.gtf.gz
+   gunzip ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.gtf.gz
+
+   cat ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.gtf \
+      | grep "^chrX" \
+      > ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.chrx.gtf
+
+   cat ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.chrx.gtf \
+      | perl -lane 'next unless $F[2] eq "transcript"; $tid = $1 if /transcript_id\s\"(.*?)\";/; print "$tid"' \
+      > ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.chrx.tid.txt
+else
+   >&2 echo ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.gtf already exists
 fi
 
-if [[ ! -e chrX_data/genes/gencode.v${ver}.transcripts.fa ]]; then
-   wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${ver}/gencode.v${ver}.transcripts.fa.gz
-   gunzip gencode.v${ver}.transcripts.fa.gz
-   ../scripts/extract_fasta.pl -i chrX_data/genes/gencode.v37.annotation.chrx.tid.txt -f gencode.v${ver}.transcripts.fa > gencode.v${ver}.transcripts.chrx.fa
-   mv gencode.v${ver}.transcripts.fa gencode.v${ver}.transcripts.chrx.fa chrX_data/genes/
+if [[ ! -e ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.transcripts.fa ]]; then
+   wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${GENCODE_VER}/gencode.v${GENCODE_VER}.transcripts.fa.gz -O ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.transcripts.fa.gz
+   gunzip ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.transcripts.fa.gz
+
+   ${DIR}/extract_fasta.pl -i ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.annotation.chrx.tid.txt -f ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.transcripts.fa > ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.transcripts.chrx.fa
+else
+   >&2 echo ${CHRX_DIR}/genes/gencode.v${GENCODE_VER}.transcripts.fa already exists
 fi
 
 >&2 echo Done
