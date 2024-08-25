6fe7dc4033927a308b4e6eb4c1ee46b0e774887e
Dave Tang <davetingpongtang@gmail.com>
2024-08-25 17:29:33 +0900

    Run RStudio Server

---
+++ scripts/run_rstudio.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+version=4.4.0
+rstudio_image=davetang/rstudio:${version}
+container_name=rstudio_server_${version}
+port=8877
+package_dir=${HOME}/r_packages_${version}
+
+if [[ ! -d ${package_dir} ]]; then
+   mkdir ${package_dir}
+fi
+
+docker run \
+   --name ${container_name} \
+   -d \
+   -p ${port}:8787 \
+   -v ${package_dir}:/packages \
+   -v ${HOME}/github/:/home/rstudio/work \
+   -e PASSWORD=password \
+   -e USERID=$(id -u) \
+   -e GROUPID=$(id -g) \
+   ${rstudio_image}
+
+>&2 echo ${container_name} listening on port ${port}
+
+exit 0
